49bcf3a577f7eee0189455893f3648803b44c711
GDeLaurentis <g.dl@hotmail.it>
2025-04-22 20:24:53 +0100

    FieldExtension at package level

--- pyadic/__init__.py
+++ pyadic/__init__.py
@@ -1,3 +1,4 @@
 from .padic import PAdic                          # noqa
 from .finite_field import ModP, rationalise       # noqa
 from .gaussian_rationals import GaussianRational  # noqa
+from .field_extension import FieldExtension       # noqa
